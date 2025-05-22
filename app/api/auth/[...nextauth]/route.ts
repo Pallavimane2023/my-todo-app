d82c53f8bf82fdf15c8844e54f13487dc0ca252f
pallavi-mane_globant <pallavi.mane@globant.com>
2025-05-22 16:36:07 +0530

    fixing next auth error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
-import { NextAuthOptions } from 'next-auth';
+import type { NextAuthOptions } from 'next-auth';
 import { connectDB } from '@/lib/mongodb';
 import User from '@/models/user';
 
